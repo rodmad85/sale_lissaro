0a2031c6adc8f84a3e152e3138ccf0691375db0f
rodrigo <rodrigomadu85@gmail.com>
2025-03-12 10:59:24 -0300

    teste

--- models/sale_order.py
+++ models/sale_order.py
@@ -11,6 +11,28 @@ class SaleLissaro(models.Model):
         ('processing', 'Processando')
     ], string='Status Entrega', compute='_compute_delivery_status', store=True,
         readonly=True, copy=False, default='nothing')
+
+    class SaleLissaro(models.Model):
+        _inherit = "sale.order"
+
+        delivery_status = fields.Selection(selection=[
+            ('nothing', 'Sem Entrega'), ('to_deliver', 'Para Entregar'),
+            ('partial', 'Parcialmente Entregue'), ('delivered', 'Entregue'),
+            ('processing', 'Processando')
+        ], string='Status Entrega', compute='_compute_delivery_status', store=True,
+            readonly=True, copy=False, default='nothing')
+        product_kanban = fields.Many2many(
+            "product.product",
+            string="Products",
+            compute="_compute_product_ids",
+            store=True
+        )
+
+        @api.depends("order_line.product_id")
+        def _compute_product_ids(self):
+            for order in self:
+                products = order.order_line.mapped("product_id")
+                order.product_kanban = [(6, 0, products.ids)] if products else [(6, 0, [])]  # Evita valores None
 class SaleOrderLissaro(models.Model):
     _inherit = "sale.order.line"
 
